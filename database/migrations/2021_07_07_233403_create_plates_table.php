a0a24b9dffb5cf73ab35e067f1af8c92c7b3e66d
182Marco <marcomilza@gmail.com>
2021-07-08 18:28:35 +0200

    fix. updated plates table to actual db scheme

--- database/migrations/2021_07_07_233403_create_plates_table.php
+++ database/migrations/2021_07_07_233403_create_plates_table.php
@@ -15,15 +15,11 @@ class CreatePlatesTable extends Migration
     {
         Schema::create('plates', function (Blueprint $table) {
             $table->id();
-            $table->string('plate_name', 50); 
+            $table->string('name', 50); 
             $table->text('ingredients'); 
             $table->boolean('visible')->default(1); 
             $table->float('price', 5, 2); 
-            $table->boolean('vegan')->default(0); // può un valore booleano essere nullable?? 
-            $table->boolean('gluten_free')->default(0); 
-            $table->boolean('dairy_ingredients')->default(0); 
             $table->string('plate_img')->nullable(); 
-            // ci servirà uno slug??? 
             $table->timestamps();
         });
     }
