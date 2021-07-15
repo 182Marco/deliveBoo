e64d7ec75b99a2b74a128a4f40b0b6a20f547d41
182Marco <marcomilza@gmail.com>
2021-07-15 18:28:09 +0200

    fixed.error in console searching #root in backEnd views

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -17,6 +17,9 @@ export default {
     components: {
         Header,
         Footer
+    },
+    created() {
+        console.log(axios);
     }
 };
 </script>
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <!-- Scripts -->
+        <script src="{{ asset('js/vue.js') }}" defer></script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
         <title>Delivero</title>
     </head>
     <body>
         <div id="root"></div>
     </body>
-    <script src="{{asset('js/app.js')}}"></script>
 </html>
